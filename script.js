90bc31ea805c1375419628347a1b12f742f93379
R-K-96 <124651427+R-K-96@users.noreply.github.com>
2023-05-17 14:31:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+// const btn = document.getElementById("enterBtn");
+
+// btn.addEventListener("click",()=>
+// 	document.getElementById("status").innerText = "";
+// 	document.querySelector("h1").innerText = "Entered Metaverse";
+// );
+function display(){
+	document.getElementById("status").innerText = "";
+	document.querySelector("h1").innerText = "Entered Metaverse";
+	
+}
+
+document.getElementById("enterBtn").addEventListener("click", display);
